8979d3dc603c57bcb061cf049da89fb6206f979c
Evgeniya Bystrikova <byseum@mail.ru>
2023-06-08 20:45:16 +0300

    добавлен вывод в консоль

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-try {
-  const body = document.querySelector('body')
-
-  document.addEventListener('click', () => {
-    e.preventDefault()
-    console.log('click');
-  });
-} catch (error) {
-  console.error(error);
-}
+console.log("Hello world!")
